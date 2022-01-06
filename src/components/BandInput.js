09afd8b03c4c9b729fdb9b587d9c12e4efbc1d6a
Doug Jones <DSJR1284@GMAIL.COM>
2022-01-06 10:40:30 -0500

    commit tree 215a124d204a9daa9d4a7d4958901b3b2e123df1

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -2,10 +2,37 @@
 import React, { Component } from 'react'
 
 class BandInput extends Component {
+
+  // set the inital state
+  state = {
+    name: ""
+  }
+
+  handleOnChange(event) {
+    //takes in the value and updates thw sate based on user input 
+    this.setState({
+      name: event.target.value 
+    })
+  }
+
+
+  handleOnSubmit(event) {
+    //update the redux state when the user hits the submit button
+    event.preventDefault();
+    this.props.addBand(this.state);
+    this.setState({
+      name: ""
+    })
+  }
+
   render() {
     return(
       <div>
         Band Input
+        <form onSubmit ={(event => this.handleOnSubmit(event))}>
+          <input type ="text" value={this.state.name} onChange= {(event) => this.handleOnChange(event)} />
+          <input type="submit" />
+        </form>
       </div>
     )
   }
